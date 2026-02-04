347e3851d7cdabee93b90acf235982d1e7b4d99a
Namhai <namhai456n@gmail.com>
2026-02-04 12:14:00 +0700

    Fix bug and update UI

--- JS/show.js
+++ JS/show.js
@@ -30,4 +30,45 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+    // === Slider Image ===
+    const track = document.querySelector('.slide-track');
+    const slides = document.querySelectorAll('.slide-track img');
+    const nextBtn = document.querySelector('.next');
+    const prevBtn = document.querySelector('.prev');
+
+    let index = 0;
+    update_title();
+    function updateSlide() {
+        track.style.transform = `translateX(-${index * 100}%)`;
+        update_title();
+    }
+
+    function update_title() {
+        const title_hero = document.getElementById('title-hero');
+        const p_hero = document.getElementById('p-hero');
+
+        if (!title_hero || !p_hero) {
+            alert("Không tìm thấy phần tử tiêu đề hoặc đoạn văn!");
+        };
+
+        if (index === 0) {
+            title_hero.innerText = "Chào mừng đến với Hội Trợ Xuân!";
+            p_hero.innerText = "Theo dõi chúng mình làm những chiếc bánh chưng ngon khó cưỡng trong cuộc thi nhé!";
+        } else {
+            title_hero.innerText = "Trải nghiệm ẩm thực đặc sắc!";
+            p_hero.innerText = "Khám phá các món ăn truyền thống và hiện đại tại Hội Trợ Xuân.";
+        }
+    }
+
+
+    nextBtn.onclick = () => {
+        index = (index + 1) % slides.length;
+        updateSlide();
+    };
+
+    prevBtn.onclick = () => {
+        index = (index - 1 + slides.length) % slides.length;
+        updateSlide();
+    };
+
 });
